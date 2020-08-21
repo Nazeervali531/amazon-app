46c1d1399e5ba481687d8cd27ec4e0a1d43d2289
Nazeervali531 <68688223+Nazeervali531@users.noreply.github.com>
2020-08-21 12:03:45 +0530

    Updates

---
+++ precache-manifest.e16c66642225763078caa89988d5e572.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a559285eaed29f67d05cce808d79c44f",
+    "url": "/amazon-app/index.html"
+  },
+  {
+    "revision": "8b7de492fa06c48c7cb2",
+    "url": "/amazon-app/static/css/main.8a1dda84.chunk.css"
+  },
+  {
+    "revision": "5f87b80efd2a1150a834",
+    "url": "/amazon-app/static/js/2.eee35344.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/amazon-app/static/js/2.eee35344.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b7de492fa06c48c7cb2",
+    "url": "/amazon-app/static/js/main.41d336e1.chunk.js"
+  },
+  {
+    "revision": "6d1d3ba958140069d071",
+    "url": "/amazon-app/static/js/runtime-main.1113cd41.js"
+  },
+  {
+    "revision": "32d584a149d341d88d4029788582b23a",
+    "url": "/amazon-app/static/media/mypic.32d584a1.jpg"
+  }
+]);
